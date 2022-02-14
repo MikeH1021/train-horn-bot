d570bcfcabbf2cd81e0aa4a430da6e281854b589
Michael Hernandez <mike@Michaels-MacBook-Pro.local>
2022-02-14 17:37:37 -0500

    time

--- lcd.py
+++ lcd.py
@@ -1,15 +1,18 @@
 from signal import signal, SIGTERM, SIGHUP, pause
 from rpi_lcd import LCD
+from datetime import datetime
+from pytz import timezone
+
 lcd = LCD()
+
+dt = datetime.now(timezone("US/Eastern"))
+
 def safe_exit(signum, frame):
     exit(1)
 try:
     signal(SIGTERM, safe_exit)
     signal(SIGHUP, safe_exit)
-    lcd.text("John Kowal is a simp", 1)
-    lcd.text("and he sucks ass", 2)
-    lcd.text("hope he chokes on", 3)
-    lcd.text("cum covered hot dogs", 4)
+    lcd.text(str(dt), 1)
     pause()
 except KeyboardInterrupt:
     pass
