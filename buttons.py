2f1af963e46978a66b1650dcee98a377ef416c33
Michael Hernandez <mike@Michaels-MacBook-Pro.local>
2022-02-14 18:19:55 -0500

    led

--- buttons.py
+++ buttons.py
@@ -6,10 +6,11 @@ led = 19
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(led, GPIO.OUT)
 
-
+while True:
 GPIO.output(led, GPIO.HIGH)
 print("Led turned on.")
-time.sleep(10)
+time.sleep(1)
 GPIO.output(led, GPIO.LOW)
 print("Led off.")
+time.sleep(1)
 GPIO.cleanup()
