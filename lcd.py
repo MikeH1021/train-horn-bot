410d6783f98ad80da0a13929a30a23bed2aa5370
Michael Hernandez <mike@Michaels-MacBook-Pro.local>
2022-02-14 17:16:22 -0500

    breaking johns will to live

--- lcd.py
+++ lcd.py
@@ -7,9 +7,9 @@ try:
     signal(SIGTERM, safe_exit)
     signal(SIGHUP, safe_exit)
     lcd.text("John Kowal is a simp", 1)
-    lcd.text("whoever gave birth to him", 2)
-    lcd.text("needs to reconsider life", 3)
-    lcd.text("hope he chokes on saliva", 4)
+    lcd.text("and he sucks ass", 2)
+    lcd.text("hope he chokes on", 3)
+    lcd.text("cum covered hot dogs", 4)
     pause()
 except KeyboardInterrupt:
     pass
