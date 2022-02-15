f71e7730656c569e0cbed7b553699fccff1e0069
Michael Hernandez <mike@Michaels-MacBook-Pro.local>
2022-02-14 23:00:34 -0500

    led

--- buttons.py
+++ buttons.py
@@ -7,7 +7,7 @@ led3 = 5
 led4 = 12
 
 sw1 = 16
-sw2 = 0
+sw2 = 18
 sw3 = 19
 sw4 = 26
 
@@ -21,13 +21,13 @@ GPIO.setup(led4, GPIO.OUT)
 while True:
     #turn on leds
     GPIO.output(led1, GPIO.HIGH)
-    GPIO.output(led2, GPIO.HIGH)
-    GPIO.output(led3, GPIO.HIGH)
-    GPIO.output(led4, GPIO.HIGH)
+    #GPIO.output(led2, GPIO.HIGH)
+    #GPIO.output(led3, GPIO.HIGH)
+    #GPIO.output(led4, GPIO.HIGH)
     #turn off leds
     time.sleep(1)
     GPIO.output(led1, GPIO.LOW)
-    GPIO.output(led2, GPIO.LOW)
-    GPIO.output(led3, GPIO.LOW)
-    GPIO.output(led4, GPIO.LOW)
+    #GPIO.output(led2, GPIO.LOW)
+    #GPIO.output(led3, GPIO.LOW)
+    #GPIO.output(led4, GPIO.LOW)
     time.sleep(1)
