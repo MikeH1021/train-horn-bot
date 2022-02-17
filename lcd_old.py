d0279c39ae64ef52368f992cb3e2a29610ab5c20
Michael Hernandez <mike@Michaels-MacBook-Pro.local>
2022-02-16 22:33:16 -0500

    added back old lcd code for testing

--- lcd_old.py
+++ lcd_old.py
@@ -1,4 +1,5 @@
 from signal import signal, SIGTERM, SIGHUP, pause
+import time
 from rpi_lcd import LCD
 from datetime import datetime
 from pytz import timezone
@@ -18,10 +19,12 @@ while True:
     signal(SIGHUP, safe_exit)
     
     if "16:20" in dt or "04:20" in dt:
+        print("smokeweed")
         lcd.text("smoke weed everyday", 1)
         sleep(5)
         lcd.clear()
     else:
+        print("time")
         lcd.text(dt, 1)
         sleep(1)
         lcd.clear()
