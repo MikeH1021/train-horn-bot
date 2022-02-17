07a619f92e3a0d0ef6c65e6e447f5464cc8270ed
Michael Hernandez <mike@Michaels-MacBook-Pro.local>
2022-02-16 20:02:51 -0500

    made lights prettier

---
+++ press.py
@@ -0,0 +1,28 @@
+import RPi.GPIO as GPIO
+import time
+
+sw1 = 16
+sw2 = 18
+sw3 = 19
+sw4 = 26
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(sw1, GPIO.IN)
+GPIO.setup(sw2, GPIO.IN)
+GPIO.setup(sw3, GPIO.IN)
+GPIO.setup(sw4, GPIO.IN)
+
+while True:
+  if (GPIO.input(sw1)):
+    print("Button 1 Pressed")
+
+    elif (GPIO.input(sw2)):
+    print("Button 2 Pressed")
+
+    elif (GPIO.input(sw3)):
+    print("Button 3 Pressed")
+
+    elif (GPIO.input(sw4)):
+    print("Button 4 Pressed")
+    
+    
