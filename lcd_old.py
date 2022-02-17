03d0404fd521411854fa1c7a0937f26bb3ac6682
Michael Hernandez <mike@Michaels-MacBook-Pro.local>
2022-02-16 22:09:33 -0500

    added back old lcd code for testing

--- lcd_old.py
+++ lcd_old.py
@@ -11,17 +11,17 @@ def safe_exit(signum, frame):
     exit(1)
 
 while True:
-        dt = datetime.now(timezone("US/Eastern"))
-        dt = str(dt)
-        dt = dt[:-13]
-        signal(SIGTERM, safe_exit)
-        signal(SIGHUP, safe_exit)
-        
-        if "16:20" in dt or "04:20" in dt:
-            lcd.text("smoke weed everyday", 1)
-            sleep(5)
-            lcd.clear()
-        else:
-            lcd.text(dt, 1)
-            sleep(1)
-            lcd.clear()
+    dt = datetime.now(timezone("US/Eastern"))
+    dt = str(dt)
+    dt = dt[:-13]
+    signal(SIGTERM, safe_exit)
+    signal(SIGHUP, safe_exit)
+    
+    if "16:20" in dt or "04:20" in dt:
+        lcd.text("smoke weed everyday", 1)
+        sleep(5)
+        lcd.clear()
+    else:
+        lcd.text(dt, 1)
+        sleep(1)
+        lcd.clear()
