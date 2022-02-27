276af2c5f3555d3dabf22a3a1c9720344593ecb2
Michael Hernandez <mike@Michaels-MacBook-Pro.local>
2022-02-27 17:01:07 -0500

    fixed

--- lcd2.py
+++ lcd2.py
@@ -1,12 +1,25 @@
+from signal import signal, SIGTERM, SIGHUP, pause
 from rpi_lcd import LCD
+from datetime import datetime
+from pytz import timezone
 from time import sleep
-
 lcd = LCD()
 
-lcd.text('Hello World!', 1)
-lcd.text('Raspberry Pi', 2)
-lcd.text('is really', 3, 'center')
-lcd.text('awesome', 4, 'right')
 
-sleep(5)
-lcd.clear()
+def safe_exit(signum, frame):
+    exit(1)
+while True:
+    dt = datetime.now(timezone("US/Eastern"))
+    dt = str(dt)
+    dt = dt[:-13]
+    signal(SIGTERM, safe_exit)
+    signal(SIGHUP, safe_exit)
+    
+    if "16:20" in dt or "04:20" in dt:
+        lcd.text("smoke weed everyday", 1)
+        sleep(5)
+        lcd.clear()
+    else:
+        lcd.text(dt, 1)
+        sleep(1)
+        lcd.clear()
